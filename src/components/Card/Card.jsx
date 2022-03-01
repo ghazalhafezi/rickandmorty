11fc24310a2c0c89442ac0499ad373c1dd0a23ca
Ghazal Hafezi <hafezighazal1991@gmail.com>
2022-03-01 13:00:09 +0330

    api call and pagination added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,29 @@
-import "./Card.scss";
+import "./Card.scss"
 
 const Card = ({ info }) => {
   return (
     <div className="card-item">
       <div className="card-img">
-        <img src={info.media} alt="" />
+        <img src={info.image} alt="" />
       </div>
       <div className="card-content">
         <h2 className="title">
-          <a href="#">{info.title}</a>
+          <a href="#">{info.name}</a>
         </h2>
-        <span className="sub-title">{info.subTitle}</span>
+        <span className="sub-title">
+          {info.status}-{info.species}
+        </span>
         <div className="section-info">
           <p className="info-title">Last known location:</p>
-          <span className="info-description">{info.location}</span>
+          <span className="info-description">{info.location.name}</span>
         </div>
         <div className="section-info">
           <p className="info-title">First seen in:</p>
-          <span className="info-description">{info.firstSeen}</span>
+          <span className="info-description">{info.origin.name}</span>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,17 +1,51 @@
-import Card from "../components/Card/Card";
-import Logo from "../components/Logo/Logo";
-import Nav from "../components/Navbar/Nav";
-import Footer from "../components/Footer/Footer";
-import "./Home.scss";
+import Card from "../components/Card/Card"
+import Logo from "../components/Logo/Logo"
+import Nav from "../components/Navbar/Nav"
+import Footer from "../components/Footer/Footer"
+import "./Home.scss"
+import { useEffect } from "react"
+import { useState } from "react"
 
 const Home = () => {
-  let info = {
-    title: "Rick-D99",
-    media: "https://rickandmortyapi.com/api/character/avatar/286.jpeg",
-    subTitle: "Alive - Human",
-    location: "Earth (Replacement Dimension)",
-    firstSeen: "Big Trouble in Little Sanchez",
-  };
+  const [data, setData] = useState([])
+  const [loading, isLoading] = useState(true)
+  const [error, isError] = useState(false)
+
+  const [nextData, setNextData] = useState("")
+  const [prevData, setPrevData] = useState("")
+
+  useEffect(() => {
+    fetchMethod("https://rickandmortyapi.com/api/character")
+  }, [])
+
+  const fetchMethod = (endPoint) => {
+    fetch(endPoint)
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response.results)
+        setNextData(response.info.next)
+        setPrevData(response.info.prev)
+        isLoading(false)
+      })
+      .catch((error) => {
+        isError(error)
+      })
+  }
+
+  const nextPageHandler = () => {
+    console.log("next", nextData)
+    if (nextData) {
+      fetchMethod(nextData)
+    }
+  }
+  const prevPageHandler = () => {
+    console.log("prev", prevData)
+    if (prevData) {
+      fetchMethod(prevData)
+    }
+  }
+
+  console.log(data)
 
   return (
     <div className="home">
@@ -22,20 +56,29 @@ const Home = () => {
         <Logo />
       </header>
 
+      {loading && <div className="loading">Loading...</div>}
       <div className="list">
-        <Card info={info} />
-        <Card info={info} />
-        <Card info={info} />
-        <Card info={info} />
-        <Card info={info} />
-        <Card info={info} />
-        <Card info={info} />
-        <Card info={info} />
+        {data.map((item) => (
+          <Card key={item.id} info={item} />
+        ))}
+      </div>
+      {/* next and prev btn */}
+      <div className="pagination">
+        {nextData && (
+          <button type="button" onClick={nextPageHandler}>
+            Next
+          </button>
+        )}
+        {prevData && (
+          <button type="button" onClick={prevPageHandler}>
+            Prev
+          </button>
+        )}
       </div>
       <div>
         <Footer />
       </div>
     </div>
-  );
-};
-export default Home;
+  )
+}
+export default Home
